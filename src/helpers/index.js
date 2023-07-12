93b508b4eb6db1cf4701ad810613244308a790d8
maxiluna45 <maximilianoluna3645@gmail.com>
2023-07-11 21:51:31 -0300

    Ahora si xd

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import dbValidators from './db-validar.js';
-=======
-const dbValidators = require('./db-validar');
->>>>>>> 3409789a50cf3b966a32c973f37e6211209a67e2
 
 module.exports = {
     ...dbValidators
--- src/models/index.js
+++ src/models/index.js
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
 import Feria from './feria';
 import Usuario from'./usuario';
 
 export default {
-=======
-const Feria = require('./feria');
-const Usuario = require('./usuario');
-
-
-module.exports = {
->>>>>>> 3409789a50cf3b966a32c973f37e6211209a67e2
     Feria,
     Usuario,
 

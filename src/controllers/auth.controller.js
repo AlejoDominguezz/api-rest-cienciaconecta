353dcaf4544b6c4b09facbe50d9a7f3349d5e612
maxiluna45 <maximilianoluna3645@gmail.com>
2023-07-27 17:55:36 -0300

    [CNC-27] Diseño de mail con html y css

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -75,7 +75,61 @@ export const register = async (req, res) => {
       from: 'Ciencia Conecta',
       to: user.email,
       subject: "Verifica tu cuenta de correo",
-      html: `<a href="http://localhost:5000/api/v1/auth/confirmar/${user.tokenConfirm}"> Verifica tu cuenta aquí </a>`
+      html: `
+      
+      <!DOCTYPE html>
+      <html>
+      <head>
+        <title>Verifica tu cuenta de correo</title>
+        <style>
+          .outer-container {
+            margin-top: 40px; 
+            padding-top: 40px;
+            padding-bottom: 40px;
+          }
+
+          .container {
+            max-width: 600px;
+            margin: 0 auto;
+            background-color: #fff;
+            border-radius: 10px;
+            padding: 20px;
+            box-shadow: 3px 3px 5px rgba(0, 0, 0, 0.2);
+            position: relative;
+          }
+
+          .shadow {
+            position: absolute;
+            top: -10px;
+            left: -10px;
+            right: -10px;
+            bottom: -10px;
+            box-shadow: 5px 5px 20px rgba(0, 0, 0, 0.1);
+            z-index: -1;
+          }
+          .black-text {
+            color: #000000; /* Color negro (#000000) */
+          }
+        </style>
+      </head>
+      <body style="font-family: Arial, sans-serif; text-align: center; background-color: #f0f0f0;">
+        <div class="outer-container">
+          <div class="shadow">
+            <div class="container">
+              <h2 style="color: #00ACE6;">¡Bienvenido a Ciencia Conecta!</h2>
+              <p class="black-text" style="font-size: 16px;">Estamos emocionados de tenerte como parte de nuestra comunidad</p>
+              <p class="black-text" style="font-size: 16px;">Haz clic en el siguiente botón para verificar tu cuenta:</p>
+              <a href="http://localhost:5000/api/v1/auth/confirmar/${user.tokenConfirm}" style="display: inline-block; background-color: #00ACE6; color: #fff; padding: 10px 20px; text-decoration: none; font-size: 16px; border-radius: 5px; margin-top: 20px;">Verificar cuenta</a>
+              <p style="font-size: 14px; color: #888; margin-top: 20px;">Si tienes problemas con el botón de verificación, también puedes copiar y pegar el siguiente enlace en tu navegador:</p>
+              <p style="font-size: 14px; color: #888;"><a href="http://localhost:5000/api/v1/auth/confirmar/${user.tokenConfirm}" style="color: #00ACE6; text-decoration: none;">http://localhost:5000/api/v1/auth/confirmar/${user.tokenConfirm}</a></p>
+              <img src="https://i.imgur.com/Cp5FCdR.jpg" alt="Marca de agua" style="position: absolute; bottom: 20px; right: 20px; max-width: 150px;">
+            </div>
+          </div>
+        </div>
+      </body>
+      </html>
+      
+      `
     })
 
     return res.status(201).json({ ok: true });

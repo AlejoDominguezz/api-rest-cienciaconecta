8a3575d4948283b63a2ad47e3ecaababff7f27f9
maxiluna45 <maximilianoluna3645@gmail.com>
2023-09-08 11:04:38 -0300

    [CNC-58] Corrijo error validacion fecha inicio Feria

--- src/routes/feria.route.js
+++ src/routes/feria.route.js
@@ -4,14 +4,14 @@ import { crearFeria, eliminarFeria, getFerias, modificarFeria } from '../control
 import { requireToken } from '../middlewares/requireToken.js';
 import { checkRolAuth } from "../middlewares/validar-roles.js";
 import { roles } from "../helpers/roles.js";
-import { bodyCrearFeriaValidator } from '../middlewares/validationManagerFeria.js';
+import { bodyCrearFeriaValidator, bodyModificarFeriaValidator } from '../middlewares/validationManagerFeria.js';
 
 const routerFerias = Router();
 
 //obtener todas las ferias 
 routerFerias.get('/', requireToken, checkRolAuth([roles.admin, roles.comAsesora]), getFerias)
 routerFerias.post('/', requireToken, checkRolAuth([roles.admin, roles.comAsesora]), bodyCrearFeriaValidator, crearFeria)
-routerFerias.patch('/:id', requireToken, checkRolAuth([roles.admin, roles.comAsesora]), bodyCrearFeriaValidator, modificarFeria)
+routerFerias.patch('/:id', requireToken, checkRolAuth([roles.admin, roles.comAsesora]), bodyModificarFeriaValidator, modificarFeria)
 routerFerias.delete('/:id', requireToken, checkRolAuth([roles.admin, roles.comAsesora]), eliminarFeria)
 
 

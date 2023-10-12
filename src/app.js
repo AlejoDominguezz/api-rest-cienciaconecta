88406fce9f2f331f025045e201320b5e151d9c9d
Alejo Dominguez <alejomariodominguez@gmail.com>
2023-10-12 10:19:12 -0300

    credentiasl ssl

--- src/app.js
+++ src/app.js
@@ -1,8 +1,25 @@
 import 'dotenv/config';
 import Server from './models/server.js';
-
+import https from 'https';
+import http from 'http';
+import fs from 'fs';
 
 const server = new Server();
 
+if (process.env.MODO === 'production') {
+    // Lee el certificado y la clave privada para HTTPS
+    const privateKey = fs.readFileSync('src/private.key', 'utf8');
+    const certificate = fs.readFileSync('src/certificate.crt', 'utf8');
+    const credentials = { key: privateKey, cert: certificate };
+
+    // Crea un servidor HTTPS con los certificados
+    const httpsServer = https.createServer(credentials, server.app);
+
+    httpsServer.listen();
+} else {
+    // En desarrollo, utiliza HTTP en el puerto 80
+    const httpServer = http.createServer(server.app);
+    httpServer.listen();
+}
 
 server.listen();

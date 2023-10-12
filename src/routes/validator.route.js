0b5cda08638b6a6318e78c12c453d29dc19752d4
Alejo Dominguez <alejomariodominguez@gmail.com>
2023-10-12 00:43:52 -0300

    test arc

--- src/routes/validator.route.js
+++ src/routes/validator.route.js
@@ -8,7 +8,7 @@ const routerValidator = Router();
 
 
 
-routerValidator.get("/9768409BDCD16F7607D59CC499BC0FC1.txt" , requireToken, (req , res) => {
+routerValidator.get("/9768409BDCD16F7607D59CC499BC0FC1.txt" , (req , res) => {
     const fileContent = file.toString(); // Convierte el búfer a una cadena de texto
     res.send(fileContent); // Envía el contenido como respuesta
 });

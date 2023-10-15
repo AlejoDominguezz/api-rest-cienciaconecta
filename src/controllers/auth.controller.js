9d93d2494fb80d83a0dd7530c9ac387fb61940ad
maxiluna45 <maximilianoluna3645@gmail.com>
2023-10-15 19:21:55 -0300

    Cambio atributos 4 logout

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -104,7 +104,7 @@ export const register = async (req, res) => {
 export const logout = (req, res) => {
   res.clearCookie("refreshToken", {
     //domain: process.env.COOKIE_ORIGIN,
-    domain: 'https://54.90.160.149.nip.io',
+    domain: '54.90.160.149.nip.io',
     path: '/',
     httpOnly: true,
     sameSite: 'none',
